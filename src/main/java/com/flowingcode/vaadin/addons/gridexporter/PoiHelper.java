ccb9122231bf649b0e0bf0d853d07243ea933571
Martín López <mlopez@flowingcode.com>
2022-08-18 14:22:02 -0300

    refactor: extract string constants

--- src/main/java/com/flowingcode/vaadin/addons/gridexporter/PoiHelper.java
+++ src/main/java/com/flowingcode/vaadin/addons/gridexporter/PoiHelper.java
@@ -16,6 +16,16 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblWidth;
  */
 public class PoiHelper {
 
+  private static final String XWPF_TABLE_CELL_CLASS_NAME = "XWPFTableCell";
+  private static final String CT_TBL_GRID_COL_CLASS_NAME = "CTTblGridCol";
+  private static final String CT_TBL_WIDTH_CLASS_NAME = "CTTblWidth";
+  private static final String CELL_CLASS_NAME = "Cell";
+  private static final String SET_WIDTH_METHOD_NAME = "setWidth";
+  private static final String SET_W_METHOD_NAME = "setW";
+  private static final String GET_CELL_TYPE_METHOD_NAME = "getCellType";
+  private static final String SET_BLANK_METHOD_NAME = "setBlank";
+  private static final String PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE = "Problem when calling method %s() on class %s";
+
   /**
    * Tries to set the cell to be blank
    * @param cell
@@ -23,12 +33,12 @@ public class PoiHelper {
   @SuppressWarnings("deprecation")
   public static void setBlank(Cell cell) {
     try {
-      Method setBlank = Cell.class.getMethod("setBlank", Void.class);
+      Method setBlank = Cell.class.getMethod(SET_BLANK_METHOD_NAME);
       setBlank.invoke(cell);
     } catch (NoSuchMethodException e) {
       cell.setCellType(CellType.BLANK);
     } catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      throw new IllegalStateException("Problem when calling setBlank() method",e);
+      throw new IllegalStateException(String.format(PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE, SET_BLANK_METHOD_NAME, CELL_CLASS_NAME),e);
     }
   }
 
@@ -42,14 +52,14 @@ public class PoiHelper {
   public static boolean cellTypeEquals(Cell cell, CellType cellType) {
     boolean result = false;
     try {
-      Method getCellType = Cell.class.getMethod("getCellType");
+      Method getCellType = Cell.class.getMethod(GET_CELL_TYPE_METHOD_NAME);
       if (getCellType.getReturnType().isPrimitive()) {
         result = getCellType.invoke(cell).equals(cellType.getCode());
       } else {
         result = getCellType.invoke(cell).equals(cellType);
       }
     } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      throw new IllegalStateException("Problem when calling setBlank() method",e);
+      throw new IllegalStateException(String.format(PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE, GET_CELL_TYPE_METHOD_NAME, CELL_CLASS_NAME),e);
     }
 
     return result;
@@ -62,17 +72,17 @@ public class PoiHelper {
    */
   public static void setWonCTTblWidth(CTTblWidth tblW, String string) {
     try {
-      Method setW = CTTblWidth.class.getMethod("setW", Object.class);
+      Method setW = CTTblWidth.class.getMethod(SET_W_METHOD_NAME, Object.class);
       setW.invoke(tblW, string);      
     } catch (NoSuchMethodException e) {
       try {
-        Method setWBigInteger = CTTblWidth.class.getMethod("setW", BigInteger.class);
+        Method setWBigInteger = CTTblWidth.class.getMethod(SET_W_METHOD_NAME, BigInteger.class);
         setWBigInteger.invoke(tblW, new BigInteger(string));
       } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
-        throw new IllegalStateException("Problem when calling setW() method on CTTblWidth",e1);
+        throw new IllegalStateException(String.format(PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE, SET_W_METHOD_NAME, CT_TBL_WIDTH_CLASS_NAME),e1);
       }
     } catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      throw new IllegalStateException("Problem when calling setW() method on CTTblWidth",e);
+      throw new IllegalStateException(String.format(PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE, SET_W_METHOD_NAME, CT_TBL_WIDTH_CLASS_NAME),e);
     }
   }
 
@@ -83,17 +93,17 @@ public class PoiHelper {
    */
   public static void setWonCTTblGridCol(CTTblGridCol cctblgridcol, String string) {
     try {
-      Method setW = CTTblGridCol.class.getMethod("setW", Object.class);
+      Method setW = CTTblGridCol.class.getMethod(SET_W_METHOD_NAME, Object.class);
       setW.invoke(cctblgridcol, string);      
     } catch (NoSuchMethodException e) {
       try {
-        Method setWBigInteger = CTTblGridCol.class.getMethod("setW", BigInteger.class);
+        Method setWBigInteger = CTTblGridCol.class.getMethod(SET_W_METHOD_NAME, BigInteger.class);
         setWBigInteger.invoke(cctblgridcol, new BigInteger(string));
       } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
-        throw new IllegalStateException("Problem when calling setW() method on CTTblGridCol",e1);
+        throw new IllegalStateException(String.format(PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE, SET_W_METHOD_NAME, CT_TBL_GRID_COL_CLASS_NAME),e1);
       }
     } catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      throw new IllegalStateException("Problem when calling setW() method on CTTblGridCol",e);
+      throw new IllegalStateException(String.format(PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE, SET_W_METHOD_NAME, CT_TBL_GRID_COL_CLASS_NAME),e);
     }
   }
 
@@ -104,12 +114,12 @@ public class PoiHelper {
    */
   public static void setWidth(XWPFTableCell currentCell, String string) {
     try {
-      Method setW = XWPFTableCell.class.getMethod("setWidth", String.class);
+      Method setW = XWPFTableCell.class.getMethod(SET_WIDTH_METHOD_NAME, String.class);
       setW.invoke(currentCell, string);      
     } catch (NoSuchMethodException e) {
-      // por ahora no hacer nada y vemos
+      // ignoring for now
     } catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-      throw new IllegalStateException("Problem when calling setW() method on CTTblGridCol",e);
+      throw new IllegalStateException(String.format(PROBLEM_WHEN_CALLING_METHOD_ON_EXCEPTION_MESSAGE, SET_WIDTH_METHOD_NAME, XWPF_TABLE_CELL_CLASS_NAME),e);
     }
   }
 
