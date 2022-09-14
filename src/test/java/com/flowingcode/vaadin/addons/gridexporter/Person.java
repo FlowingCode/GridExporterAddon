2ad6a38dd081fc97124cf3cdcd813c5ab78d5b2c
Martín López <mlopez@flowingcode.com>
2022-09-14 18:49:00 -0300

    feat: add support for BasicRenderers such as LocalDateRenderer
    Close #11

--- src/test/java/com/flowingcode/vaadin/addons/gridexporter/Person.java
+++ src/test/java/com/flowingcode/vaadin/addons/gridexporter/Person.java
@@ -1,18 +1,26 @@
 package com.flowingcode.vaadin.addons.gridexporter;
 
+import java.time.LocalDate;
+
 public class Person {
 
 	private String name;
 	private String lastName;
 	private Integer age;
 	private Double budget;
+	private LocalDate favDate;
 	
-	public Person(String name, String lastName, Integer age, Double budget) {
+    public Person(String name, String lastName, Integer age, Double budget) {
+      this(name,lastName,age,budget,null);
+    }
+
+    public Person(String name, String lastName, Integer age, Double budget, LocalDate favDate) {
 		super();
 		this.name = name;
 		this.lastName = lastName;
 		this.age = age;
 		this.budget = budget;
+		this.favDate = favDate;
 	}
 	public String getName() {
 		return name;
@@ -38,5 +46,11 @@ public class Person {
 	public void setBudget(Double budget) {
 		this.budget = budget;
 	}
+    public LocalDate getFavDate() {
+        return favDate;
+    }
+    public void setFavDate(LocalDate favoriteDate) {
+        this.favDate = favoriteDate;
+    }
 	
 }
