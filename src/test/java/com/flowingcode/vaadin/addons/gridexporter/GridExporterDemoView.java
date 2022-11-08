771cbf01a0fcafc78497f5c3c3759839e60dc9e5
Martín López <mlopez@flowingcode.com>
2022-11-08 18:29:42 -0300

    fix(demo): use @ParentLayout instead of just @Route

--- src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemoView.java
@@ -22,10 +22,12 @@ package com.flowingcode.vaadin.addons.gridexporter;
 import com.flowingcode.vaadin.addons.DemoLayout;
 import com.flowingcode.vaadin.addons.GithubLink;
 import com.flowingcode.vaadin.addons.demo.TabbedDemo;
+import com.vaadin.flow.router.ParentLayout;
 import com.vaadin.flow.router.Route;
 
 @SuppressWarnings("serial")
-@Route(value = "gridexporter", layout = DemoLayout.class)
+@ParentLayout(DemoLayout.class)
+@Route("gridexporter")
 @GithubLink("https://github.com/FlowingCode/GridExporterAddon")
 public class GridExporterDemoView extends TabbedDemo {
 
