88f24c8ee70e23af501d196aab27f495b0e3f33c
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2023-02-06 10:36:54 -0300

    chore: update copyright headers

--- src/test/java/com/flowingcode/vaadin/addons/DemoLayout.java
+++ src/test/java/com/flowingcode/vaadin/addons/DemoLayout.java
@@ -2,7 +2,7 @@
  * #%L
  * Template Add-on
  * %%
- * Copyright (C) 2021 Flowing Code
+ * Copyright (C) 2022 - 2023 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
