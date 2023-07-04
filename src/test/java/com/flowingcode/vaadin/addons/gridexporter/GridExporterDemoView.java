5a6726257e4afbf89764a9b9ed0c1c18abac4586
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2023-07-04 16:22:52 -0300

    fix(demo): set github branch

--- src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemoView.java
@@ -20,6 +20,7 @@
 package com.flowingcode.vaadin.addons.gridexporter;
 
 import com.flowingcode.vaadin.addons.DemoLayout;
+import com.flowingcode.vaadin.addons.GithubBranch;
 import com.flowingcode.vaadin.addons.GithubLink;
 import com.flowingcode.vaadin.addons.demo.TabbedDemo;
 import com.vaadin.flow.router.ParentLayout;
@@ -29,6 +30,7 @@ import com.vaadin.flow.router.Route;
 @ParentLayout(DemoLayout.class)
 @Route("gridexporter")
 @GithubLink("https://github.com/FlowingCode/GridExporterAddon")
+@GithubBranch("1.x")
 public class GridExporterDemoView extends TabbedDemo {
 
   public GridExporterDemoView() {
