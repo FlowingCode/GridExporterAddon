ee2f59458334b725b40040122b10c5542f7aed26
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2023-07-04 14:33:17 -0300

    style: apply code formatter

--- src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterCustomLinkDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterCustomLinkDemo.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,6 @@
  */
 package com.flowingcode.vaadin.addons.gridexporter;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-import org.apache.poi.EncryptedDocumentException;
 import com.flowingcode.vaadin.addons.demo.DemoSource;
 import com.github.javafaker.Faker;
 import com.vaadin.flow.component.grid.Grid;
@@ -35,6 +28,13 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.apache.poi.EncryptedDocumentException;
 
 @DemoSource
 @PageTitle("Grid Exporter Addon Custom Link Demo")
@@ -47,24 +47,32 @@ public class GridExporterCustomLinkDemo extends Div {
     grid.removeAllColumns();
     grid.addColumn("name").setHeader("Name");
     grid.addColumn("lastName").setHeader("Last Name");
-    Column<Person> c = grid.addColumn(item->"$" + item.getBudget()).setHeader("Budget");
+    Column<Person> c = grid.addColumn(item -> "$" + item.getBudget()).setHeader("Budget");
     BigDecimal[] total = new BigDecimal[1];
     total[0] = BigDecimal.ZERO;
-    Stream<Person> stream = IntStream.range(0, 100).asLongStream().mapToObj(number->{
-        Faker faker = new Faker();
-        Double budget = faker.number().randomDouble(2, 10000, 100000);
-        total[0] = total[0].add(BigDecimal.valueOf(budget));
-        c.setFooter("$" + total[0]);
-        return new Person(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(15, 50)
-        		, budget);
-    });
+    Stream<Person> stream =
+        IntStream.range(0, 100)
+            .asLongStream()
+            .mapToObj(
+                number -> {
+                  Faker faker = new Faker();
+                  Double budget = faker.number().randomDouble(2, 10000, 100000);
+                  total[0] = total[0].add(BigDecimal.valueOf(budget));
+                  c.setFooter("$" + total[0]);
+                  return new Person(
+                      faker.name().firstName(),
+                      faker.name().lastName(),
+                      faker.number().numberBetween(15, 50),
+                      budget);
+                });
     grid.setItems(DataProvider.fromStream(stream));
     grid.setWidthFull();
     this.setSizeFull();
     GridExporter<Person> exporter = GridExporter.createFor(grid);
     exporter.setAutoAttachExportButtons(false);
     exporter.setTitle("People information");
-    exporter.setFileName("GridExport" + new SimpleDateFormat("yyyyddMM").format(Calendar.getInstance().getTime()));
+    exporter.setFileName(
+        "GridExport" + new SimpleDateFormat("yyyyddMM").format(Calendar.getInstance().getTime()));
     Anchor excelLink = new Anchor("", "Export to Excel");
     excelLink.setHref(exporter.getExcelStreamResource());
     excelLink.getElement().setAttribute("download", true);
--- src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemo.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,15 +19,6 @@
  */
 package com.flowingcode.vaadin.addons.gridexporter;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import org.apache.poi.EncryptedDocumentException;
-
 import com.flowingcode.vaadin.addons.demo.DemoSource;
 import com.github.javafaker.Faker;
 import com.vaadin.flow.component.grid.Grid;
@@ -36,6 +27,13 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.apache.poi.EncryptedDocumentException;
 
 @DemoSource
 @PageTitle("Grid Exporter Addon Basic Demo")
@@ -52,14 +50,21 @@ public class GridExporterDemo extends Div {
     Column<Person> budgetCol = grid.addColumn(item -> "$" + item.getBudget()).setHeader("Budget");
     BigDecimal[] total = new BigDecimal[1];
     total[0] = BigDecimal.ZERO;
-    Stream<Person> stream = IntStream.range(0, 100).asLongStream().mapToObj(number->{
-        Faker faker = new Faker();
-        Double budget = faker.number().randomDouble(2, 10000, 100000);
-        total[0] = total[0].add(BigDecimal.valueOf(budget));
-        budgetCol.setFooter("$" + total[0]);
-        return new Person(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(15, 50)
-        		, budget);
-    });
+    Stream<Person> stream =
+        IntStream.range(0, 100)
+            .asLongStream()
+            .mapToObj(
+                number -> {
+                  Faker faker = new Faker();
+                  Double budget = faker.number().randomDouble(2, 10000, 100000);
+                  total[0] = total[0].add(BigDecimal.valueOf(budget));
+                  budgetCol.setFooter("$" + total[0]);
+                  return new Person(
+                      faker.name().firstName(),
+                      faker.name().lastName(),
+                      faker.number().numberBetween(15, 50),
+                      budget);
+                });
     grid.setItems(DataProvider.fromStream(stream));
     grid.setWidthFull();
     this.setSizeFull();
@@ -67,7 +72,8 @@ public class GridExporterDemo extends Div {
     exporter.setExportValue(budgetCol, item -> "" + item.getBudget());
     exporter.setColumnPosition(lastNameCol, 1);
     exporter.setTitle("People information");
-    exporter.setFileName("GridExport" + new SimpleDateFormat("yyyyddMM").format(Calendar.getInstance().getTime()));
+    exporter.setFileName(
+        "GridExport" + new SimpleDateFormat("yyyyddMM").format(Calendar.getInstance().getTime()));
     add(grid);
   }
 }
