405daefbcd1679f10a4937e7a54bee780845ae7b
Felipe Lang <felipe.lang@ext.vaadin.com>
2023-11-29 10:44:12 -0300

    refactor: make class serializable

--- src/main/java/com/flowingcode/vaadin/addons/gridexporter/FooterToolbarItem.java
+++ src/main/java/com/flowingcode/vaadin/addons/gridexporter/FooterToolbarItem.java
@@ -1,13 +1,14 @@
 package com.flowingcode.vaadin.addons.gridexporter;
 
 import com.vaadin.flow.component.Component;
+import java.io.Serializable;
 
 /**
  * Item that can be added to {@link FooterToolbar}.
  * <p>
  * Default position is {@link FooterToolbarItemPosition.AFTER_EXPORT_BUTTONS}
  */
-public class FooterToolbarItem {
+public class FooterToolbarItem implements Serializable {
 
   private final Component component;
 
