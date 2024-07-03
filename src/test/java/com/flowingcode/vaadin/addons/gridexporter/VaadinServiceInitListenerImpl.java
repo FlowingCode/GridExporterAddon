a1b99fc0fdc2dbac5d1760441a94003151699737
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2024-07-03 09:21:12 -0300

    chore(demo): highlight block in VaadinServiceInitListener

--- src/test/java/com/flowingcode/vaadin/addons/gridexporter/VaadinServiceInitListenerImpl.java
+++ src/test/java/com/flowingcode/vaadin/addons/gridexporter/VaadinServiceInitListenerImpl.java
@@ -11,11 +11,15 @@ public class VaadinServiceInitListenerImpl implements VaadinServiceInitListener 
   @Override
   public void serviceInit(ServiceInitEvent event) {
     GridExporter.setConcurrentDownloadLimit(10);
+
+    // begin-block setConcurrentDownloadTimeout
     GridExporter.setConcurrentDownloadTimeout(5, TimeUnit.SECONDS);
     GridExporter.addGlobalConcurrentDownloadTimeoutEvent(ev -> {
       Notification.show("System is busy. Please try again later.")
           .addThemeVariants(NotificationVariant.LUMO_ERROR);
     });
+    // end-block
+
   }
 
 }
