5cc6d15a96cd50dff9eb858ed8534a38b5337fae
Martin Lopez <mlopez@flowingcode.com>
2024-10-15 16:21:48 -0300

    feat: add support for multiple header rows
    Add support for multiple header rows only for excel files

--- src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/gridexporter/GridExporterDemoView.java
@@ -39,6 +39,7 @@ public class GridExporterDemoView extends TabbedDemo {
     addDemo(GridExporterCustomColumnsDemo.class);
     addDemo(GridExporterHierarchicalDataDemo.class);
     addDemo(GridExporterBigDatasetDemo.class);
+    addDemo(GridExporterMultipleHeaderRowsDemo.class);
     setSizeFull();
   }
 }
